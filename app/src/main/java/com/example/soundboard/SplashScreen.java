afd1de91c31ea6d1f0f2699e0a5f63834235f594
Rekkuzan5 <cstapp88@gmail.com>
2020-02-24 21:58:07 -0600

    working on splash screen.

---
+++ app/src/main/java/com/example/soundboard/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.soundboard;
+
+import android.os.Bundle;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
